e744a4b9ac778e53957ee3ebbbb184863704285d
Anthony Zamarro <azamarro@veracode.com>
2022-01-13 18:36:09 -0500

    normalize function for escaping urls

--- script.js
+++ script.js
@@ -23,20 +23,21 @@ const getAllLinks = async () => {
 
 
 const parseJsonData = ([pageOne, pageTwo]) => {
-    const pageOneLinks = JSON.parse(pageOne)?.query.pages['49387265'].links;
-    const pageTwoLinks = JSON.parse(pageTwo)?.query.pages['49387265'].links;
-    buildUrls(pageOneLinks.concat(pageTwoLinks));
+    const pageOneTitles = JSON.parse(pageOne)?.query.pages['49387265'].links;
+    const pageTwoTitles = JSON.parse(pageTwo)?.query.pages['49387265'].links;
+    buildUrls(pageOneTitles.concat(pageTwoTitles));
 };
 
-const buildUrls = (allLinks) => {
+const buildUrls = (allTitles) => {
     // 682 links in total
-    const pageUrls = allLinks.map(link => {
-        normalize(link);
-        return `https://en.wikipedia.org/wiki/api.php?action=query&titles=${link.title}&prop=pageprops&format=json&origin=*`
+    const pageUrls = allTitles.map(title => {
+        return `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&rvprop=content&rvsection=0&titles=${normalize(title.title)}`;
+        // return `https://en.wikipedia.org/wiki/api.php?action=query&titles=${normalize(title.title)}&prop=pageprops&format=json&origin=*`
     });
+    console.log(pageUrls);
 };
 
-const normalize = (url) => {
+const normalize = (title) => {
     /* take care of 
         - spaces 
         - single quote '
@@ -45,9 +46,50 @@ const normalize = (url) => {
         - double quote -- every title is wrapped in single quotes, unless there is 
                           an apostrophe in the title
     */
+    // console.log(title);
+    const temp = []
+    for (let index = 0; index < title.length; index++) {
+        let element = title[index];
+        switch (element) {
+            case ' ':
+                element = '_';
+                break;
+            case '\'':
+                element = '%27';
+                break;
+            case '?':
+                element = '%3F';
+                break;
+            case '&':
+                element = '%26';
+                break;
+            case '"':
+                element = '%22';
+                break;
+            default:
+                break;
+        }
 
-    console.log(url)
-}
+        temp.push(element)
+    }
+    return temp.join('');
+
+};
+
+// const array =
+//     [
+//         `Bart's Dog Gets an "F"`,
+//         "Bart's Comet",
+//         "D'oh-in' in the Wind",
+//         "Dude, Where's My Ranch?",
+//         'Itchy & Scratchy & Marge'
+//     ]
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     normalize(element);
+
+// }
 
 getAllLinks();
 
@@ -55,8 +97,16 @@ getAllLinks();
 /**
  * shape of data
  * 
- * title: string - title of episode
+ * title: string - title of episode √
  * pageid: number - id of page
- * link: string - URL of wikipedia page √ 
+ * link: string - URL of wikipedia page √
+ * 
+ * 
+ * 
+ * normalize url so that fetching is faster
+ * visit each URL and get pageid
+ * store episode info in either DB or Google Sheet file
+ * 
+ * 
  * 
  */
